51e6b1a4211c67992959b0e5d620f7b5c4228781
Afshin Hoseini <afshin.hoseini@gmail.com>
2016-06-12 08:27:59 +0430

    Documented DownloadCachePolicy class.

--- netup/src/main/java/ir/afshin/netup/Requests/DownloadCachePolicy.java
+++ netup/src/main/java/ir/afshin/netup/Requests/DownloadCachePolicy.java
@@ -5,10 +5,23 @@ package ir.afshin.netup.Requests;
  */
 public class DownloadCachePolicy {
 
+    /**
+     * An constant representing the number of milliseconds of a single day.
+     */
     public static final long OneDay = 86400000;
+    /**
+     * An constant representing the number of milliseconds of a single hour.
+     */
     public static final long OneHour = 60000;
 
+
+    /**
+     * Determines if the downloading content must be kept in cached places or not.
+     */
     public boolean shouldCache = true;
+    /**
+     * Determines the maximum age of cached file, the default value is 2 days.
+     */
     public long maxAge = OneDay * 2;
 
 }
