06456f1370de60dc906b3648d497ac26dcd724d0
Afshin Hoseini <afshin.hoseini@gmail.com>
2016-06-12 10:31:21 +0430

    Documented JsonArrayRequest class.
    Also fixed a bug.

--- netup/src/main/java/ir/afshin/netup/Requests/JsonArrayRequest.java
+++ netup/src/main/java/ir/afshin/netup/Requests/JsonArrayRequest.java
@@ -3,7 +3,6 @@ package ir.afshin.netup.Requests;
 import android.content.Context;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
@@ -13,16 +12,18 @@ import ir.afshin.netup.base.Pair;
 import ir.afshin.netup.base.PostParam;
 
 /**
+ * It requests server for an json array response. If you know the api you're calling will return
+ * json array, it's the case.
  * Created by afshinhoseini on 2/8/16.
  */
 public class JsonArrayRequest extends StringRequest {
 
 
-    private OnJsonArrayResponse listener = null;
+    private OnResponse listener = null;
 
 // ____________________________________________________________________
 
-    public JsonArrayRequest(Context ctx, OnJsonArrayResponse listener) {
+    public JsonArrayRequest(Context ctx, OnResponse listener) {
 
         super(ctx, null);
         super.setOnStringResponseListener(onStringResponse);
@@ -31,7 +32,7 @@ public class JsonArrayRequest extends StringRequest {
 
 // ____________________________________________________________________
 
-    public JsonArrayRequest(Context ctx, String url, ArrayList<Pair<String, String>> Headers, ArrayList<Pair<String, String>> get_params, ArrayList<PostParam> post_params, OnJsonArrayResponse listener) {
+    public JsonArrayRequest(Context ctx, String url, ArrayList<Pair<String, String>> Headers, ArrayList<Pair<String, String>> get_params, ArrayList<PostParam> post_params, OnResponse listener) {
 
         super(ctx, url, Headers, get_params, post_params, null);
         super.setOnStringResponseListener(onStringResponse);
@@ -40,21 +41,41 @@ public class JsonArrayRequest extends StringRequest {
 
 // ____________________________________________________________________
 
-    public void setOnJsonResponseListener(OnJsonArrayResponse listener) {
+    public void setOnJsonResponseListener(OnResponse listener) {
 
         this.listener = listener;
     }
 
 // ____________________________________________________________________
 
-    public interface OnJsonArrayResponse {
+    /**
+     * Listens to {@link JsonArrayRequest json array requests} to figure out when a request starts
+     * or when finishes.
+     */
+    public interface OnResponse {
 
+        /**
+         * Will be called once the given {@link JsonArrayRequest json array request} is started.
+         * @param request The started request.
+         */
         void onStart(JsonArrayRequest request);
+
+        /**
+         * Will be called once the given {@link JsonArrayRequest json array request} is finished.
+         * @param request The finished request.
+         * @param response The json array response.
+         * @param success <i>true</i> if the request succeeded, <i>false</i> otherwise.
+         * @param connection A pointer to connection itself.
+         * @param status An instance of {@link ConnectionStatus} describing the status of connection.
+         */
         void onFinish(JsonArrayRequest request, JSONArray response, boolean success, HttpURLConnection connection, ConnectionStatus status);
     }
 
 // ____________________________________________________________________
 
+    /**
+     * Listens to the result of api string response.
+     */
     private OnStringResponse onStringResponse = new OnStringResponse() {
 
 
@@ -78,8 +99,6 @@ public class JsonArrayRequest extends StringRequest {
                 } catch (Exception e) {
 
                     e.printStackTrace();
-                    success = false;
-                    status = ConnectionStatus.UNSUCCESSFUL;
                 }
             }
 
